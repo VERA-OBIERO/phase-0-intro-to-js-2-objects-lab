fb81a6e7f58d5d814633697bcb2943dd268b5857
VERA-OBIERO <vera.obiero@gmail.com>
2023-10-15 17:42:10 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee ={emply1:{name:"Latifa", streetAddress:"kiserian"},emply2:{name:"Moha",streetAddress:"Juja"}}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]:value,
+    };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] =  value;
+    return employee;
+    };
+
+function deleteFromEmployeeByKey(employee, key){
+    const nwEmply = {...employee};
+    delete nwEmply[key];
+    return nwEmply;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee[key];
+    return employee;
+
+}
